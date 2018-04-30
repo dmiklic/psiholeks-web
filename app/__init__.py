d34cf198b6363f7eade10c9de2f4c6bec15cd4b5
Damjan Miklic <damjan.miklic@fer.hr>
2018-05-01 01:56:45 +0200

    Update for production.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 # app/__init__.py
 
+import os
+
 # third-party imports
 from flask import Flask
 from flask_bootstrap import Bootstrap
@@ -13,9 +15,16 @@ from config import app_config
 db = SQLAlchemy()
 
 def create_app(config_name):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
+    if os.getenv('FLASK_CONFIG')=="production":
+        app = Flask(__name__)
+        app.config.update(
+            SECRET_KEY=os.getenv('SECRET_KEY'),
+            SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
+        )
+    else:
+        app = Flask(__name__, instance_relative_config=True)
+        app.config.from_object(app_config[config_name])
+        app.config.from_pyfile('config.py')
 
     Bootstrap(app)
     db.init_app(app)
